444ac7884552a86bc42a142f4b2b0cd5300171fd
564424062 <564424062@qq.com>
2016-06-20 18:54:34 +0800

    dsa

--- 0620/src/com/a.java
+++ 0620/src/com/a.java
@@ -1,5 +1,12 @@
 package com;
 
-public class a {
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 
+public class a {
+	public static void main(String[] args) throws UnknownHostException {
+		InetAddress address=InetAddress.getByName("jlb");
+		System.out.println(address.getHostAddress());
+	}
 }
